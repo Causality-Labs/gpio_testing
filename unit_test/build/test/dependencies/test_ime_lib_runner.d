9824220b2e8bb75eb3f49bd8a4cb42bb6ba25d30
Causality-Labs <imeasamudo@gmail.com>
2025-01-18 17:40:25 -0700

    Working mock unit test complete

---
+++ unit_test/build/test/dependencies/test_ime_lib_runner.d
@@ -0,0 +1,5 @@
+build/gcov/out/test_ime_lib/test_ime_lib_runner.o: \
+ build/test/runners/test_ime_lib_runner.c build/vendor/unity/src/unity.h \
+ build/vendor/unity/src/unity_internals.h build/vendor/cmock/src/cmock.h \
+ build/vendor/cmock/src/cmock_internals.h ../inc/ime_lib.h \
+ build/test/mocks/test_ime_lib/mock_Display.h ../inc/Display.h
---
+++ unit_test/test/test_ime_lib.c
@@ -0,0 +1,55 @@
+#include "unity.h"
+#include "ime_lib.h"
+#include "mock_Display.h"
+
+int Draw_Int_Callback(int x, int num_calls);
+
+void setUp(void)
+{
+}
+ 
+void tearDown(void)
+{
+}
+ 
+/*******************************************************************************
+ *    TESTS
+ ******************************************************************************/
+ 
+void test_add_numbers(void)
+{
+    TEST_ASSERT_EQUAL_INT(2,add_numbers(1,1));
+}
+
+
+void test_MakePoint_creates_new_point(void)
+{
+    struct point pt = MakePoint(3,4);
+    TEST_ASSERT_EQUAL_INT(3,pt.x);
+    TEST_ASSERT_EQUAL_INT(4,pt.y);
+}
+
+void test_MakePoint_Draw_Coordinates_pass(void)
+{
+    int ret = 0;
+    Draw_Int_StubWithCallback(Draw_Int_Callback);
+    ret = init_Display(3, 4);
+    TEST_ASSERT_EQUAL_INT(0, ret);
+}
+
+void test_MakePoint_Draw_Coordinates_fail(void)
+{
+    int ret = 0;
+    Draw_Int_StubWithCallback(Draw_Int_Callback);
+    ret = init_Display(-3, -4);
+    TEST_ASSERT_EQUAL_INT(-1, ret);
+}
+
+int Draw_Int_Callback(int x, int num_calls)
+{
+    if(x < 0)
+    {
+        return -1;
+    }
+    return 0;
+}
