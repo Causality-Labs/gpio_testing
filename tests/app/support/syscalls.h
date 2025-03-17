24e79e6fbca463ea50ace66e2d9f1e09720a718c
Ime Asamudo <iasamudo@tektelic.com>
2025-03-17 16:49:22 -0600

    Shhet idk

--- tests/app/support/syscalls.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYSCALLS_H
-#define SYSCALLS_H
-
-int open(const char *pathname, int flag, ...);
-int ioctl(int fd, unsigned long request, void *arg);
-int read(int fd, void *arg, int count);
-int close(int fd);
-
-#endif // SYSCALLS_H
